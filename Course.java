d168ae8958467eef694ee3790e7dd71c8af98d22
Sajith_yd <hesoyamfsa@gmail.com>
2026-07-31 03:47:51 +0530

    Add Course and Student classes with constructors and methods

---
+++ Course.java
@@ -0,0 +1,29 @@
+public class Course {
+    private String ccode;
+    private String title;
+    public Course(String ccode, String title) {
+        this.ccode = ccode;
+        this.title = title;
+    }
+    /*
+    returns the course code 
+    @return course code
+    */
+    String getCCode() {
+        return ccode;
+    }
+    /*
+    returns the course title
+    @return course title
+    */
+    String getTitle() {
+        return title;
+    }
+    /*
+    returns a string representation of the course
+    @return string representation of the course
+    */
+    String toString() {
+        return "Course Code: " + ccode + ", Title: " + title;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+    private String id;
+    private String name;
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    } 
+    /*
+    returns the ID of the student
+    @return ID of the student
+    */
+    String getId() {
+        return id;
+    }
+    /*
+    returns the name of the student
+    @return name of the student
+    */
+    String getName() {
+        return name;
+    }
+    /*
+    returns a string representation of the student
+    @return string representation of the student
+    */
+    String toString() {
+        return "Student ID: " + id + ", Name: " + name;
+    }
+
+}
